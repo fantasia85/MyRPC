f23104dd5d73d2b4b057503dfe81a83f17d55859
hiya <541799565@qq.com>
2020-08-23 12:35:31 +0800

    HttpProtocol

--- myrpc/http/HttpProtocol.cpp
+++ myrpc/http/HttpProtocol.cpp
@@ -64,3 +64,85 @@ void URLEncode(const char *source, char *dest, size_t length) {
 
 }
 
+namespace myrpc {
+
+void HttpProtocol::FixRespHeaders(bool keep_alive, const char *version, HttpResponse *resp) {
+    char buffer[256] = {0};
+
+    //检查keep-alive头部
+    if (keep_alive) {
+        if (resp->GetHeaderValue(HttpMessage::HEADER_CONNECTION) == nullptr) {
+            resp->AddHeader(HttpMessage::HEADER_CONNECTION, "Keep-Alive");
+        }
+    }
+
+    //检查数据头部
+    resp->RemoveHeader(HttpMessage::HEADER_DATE);
+    time_t t_time = time(nullptr);
+    struct tm tm_time;
+    gmtime_r(&t_time, &tm_time);
+    strftime(buffer, sizeof(buffer), "%a, %d %b %Y %H:%M:%S %Z", &tm_time);
+    resp->AddHeader(HttpMessage::HEADER_DATE, buffer);
+
+    //检查服务头部
+    resp->RemoveHeader(HttpMessage::HEADER_SERVER);
+    resp->AddHeader(HttpMessage::HEADER_SERVER, "http/myrpc");
+
+    //设置版本 
+    resp->set_version(version);
+}
+
+void HttpProtocol::FixRespHeaders(const HttpRequest &req, HttpResponse *resp) {
+    FixRespHeaders(req.keep_alive(), req.version(), resp);
+}
+
+int HttpProtocol::SendReqHeader(BaseTcpStream &socket, const char *method, const HttpRequest &req) {
+    std::string  url;
+
+    if (req.GetParamCount() > 0) {
+        url.append(req.uri());
+        url.append("?");
+
+        char tmp[1024] = {0};
+        for (size_t i = 0; i < req.GetParamCount(); i++) {
+            if (i > 0)
+                url.append("&");
+            URLEncode(req.GetParamName(i), tmp, sizeof(tmp) - 1);
+            url.append(tmp);
+            url.append("=");
+            URLEncode(req.GetParamValue(i), tmp, sizeof(tmp) - 1);
+            url.append(tmp);
+        }
+    }
+
+    socket << method << " " << (url.size() > 0 ? url.c_str() : req.uri()) 
+            << " " << req.version() << "\r\n";
+
+    for (size_t i = 0; i < req.GetHeaderCount(); ++i) {
+        const char *name = req.GetHeaderName(i);
+        const char *val = req.GetHeaderValue(i);
+
+        socket << name << ": " << val << "\r\n";
+    }
+
+    if (req.content().size() > 0) {
+        if (req.GetHeaderValue(HttpMessage::HEADER_CONTENT_LENGTH) == nullptr) {
+            socket << HttpMessage::HEADER_CONTENT_LENGTH << ": "
+                    << req.content().size() << "\r\n";
+        }
+    }
+
+    socket << "\r\n";
+
+    if (req.content().size() == 0) {
+        if (socket.flush().good()) {
+            return 0;
+        }
+        else
+            return  static_cast<int> (socket.LastError());
+    }
+
+    return 0;
+}
+
+}
