86dc87ba17a373ccf40238430a2d665fe787581e
hiya <541799565@qq.com>
2020-08-30 16:30:30 +0800

    rpc

---
+++ myrpc/rpc.h
@@ -0,0 +1,9 @@
+/* 包含rpc文件夹下的头文件 */
+
+#pragma once 
+
+#include "rpc/myrpc.pb.h"
+#include "rpc/MyServer.h"
+#include "rpc/ServerBase.h"
+#include "rpc/ServerConfig.h"
+#include "rpc/ThreadQueue.h"
